9a5f28eac331e7f5533da2351b3440ad6324e466
张军厚 <z824481406@gmail.com>
2016-10-25 11:20:23 +0800

    no message

---
+++ iconfont.js
@@ -0,0 +1,11 @@
+/** Created by iconfont-map-builder */
+
+import createIconSet from './lib/create-icon-set';
+var iconfont =
+{"46813":58891,"jilu":58880,"qq":58916,"icon11":58926,"fanhui":58881,"dianhua":58890,"guanbi":58906,"xiala":58908,"shop":58889,"weixin":58917,"xiugai":58914,"fail":58882,"tianjia":58915,"jikediancanicon09":58920,"shangla":58927,"qushi":58884,"yaoqingma":58895,"mima":58928,"yiqueren":58923,"jinbi":58883,"address":58912,"youxi":58892,"home":58910,"weibo":58918,"jian":58921,"shenfenzheng":58913,"wancheng":58924,"liwu":58885,"duihuantext":58886,"choujiang":58887,"kefu":58893,"gengduo":58909,"kefu1":58894,"mingxi":58888,"pyq":58919,"zhanghu":58896,"duihuanjilu":58904,"tuiguang":58907,"hiinicondaiqueren":58925,"huo":58905,"gonggao":58898,"bangzhu":58897,"huodong":58899,"renwu":58900,"duihuan":58901,"ucenter":58902,"ucenter2":58903,"icon0103":58911,"jiaoyimima":58929,"gou":58922}
+
+
+let IconFont = createIconSet(iconfont, 'iconfont', 'iconfont.ttf');
+
+module.exports = IconFont;
+module.exports.iconfont = iconfont;
